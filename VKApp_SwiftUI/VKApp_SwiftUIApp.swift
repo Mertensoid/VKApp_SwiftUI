3a31cf1929a3934a7adae052e7142c3a5eaecddb
Mertensoid <matsoukm@yandex.ru>
2022-06-20 14:09:19 +0300

    Initial Commit

---
+++ VKApp_SwiftUI/VKApp_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  VKApp_SwiftUIApp.swift
+//  VKApp_SwiftUI
+//
+//  Created by admin on 20.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct VKApp_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
